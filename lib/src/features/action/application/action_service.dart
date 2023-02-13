4892a7ce4b49350d4448039c346dca2a44807642
Zachary Taylor <ztaylor54@gmail.com>
2023-02-12 20:20:05 -0600

    Implement runAction

--- lib/src/features/action/application/action_service.dart
+++ lib/src/features/action/application/action_service.dart
@@ -1,6 +1,7 @@
 import 'package:app_profiles/src/features/action/data/action_repository.dart';
 import 'package:app_profiles/src/features/action/models/action/action.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:process_run/shell.dart';
 
 /// The action service.
 class ActionService {
@@ -8,19 +9,26 @@ class ActionService {
     required this.actionRepository,
   });
 
+  final _shell = Shell();
+
   final ActionRepository actionRepository;
-  
+
   /// Get an [Action] by ID.
-  Future<Action> getAction(ActionId actionId) => actionRepository.getAction(actionId);
+  Future<Action> getAction(ActionId actionId) =>
+      actionRepository.getAction(actionId);
 
   /// Save an [Action].
   Future<void> saveAction(Action action) => actionRepository.saveAction(action);
 
   /// Delete an [Action] by ID.
-  Future<void> deleteAction(ActionId actionId) => actionRepository.deleteAction(actionId);
+  Future<void> deleteAction(ActionId actionId) =>
+      actionRepository.deleteAction(actionId);
 
   /// Run an [Action] by ID.
-  Future<void> runAction(ActionId actionId) => throw UnimplementedError();
+  Future<void> runAction(ActionId actionId) async =>
+      actionRepository.getAction(actionId).then(
+            (action) => _shell.run(action.command),
+          );
 }
 
 final actionServiceProvider = Provider<ActionService>((ref) {
